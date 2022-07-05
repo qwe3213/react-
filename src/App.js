0a3f6b79f72bc96d4b7e205f82baacd209582e6f
hayoon <hs183000@naver.com>
2022-07-05 17:47:10 +0900

    restart

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import {useState} from "react";
+function App(){
+      const [counter,setValue] = useState(0);
+      const onClick = ()=> setValue((prev)=>prev+1);
+  return( 
+     
+  <div >
+    <h1> {counter}</h1>
+    <button onClick={onClick}>click module</button>
+    </div>
+  
+  );
+}
+
+export default App;
